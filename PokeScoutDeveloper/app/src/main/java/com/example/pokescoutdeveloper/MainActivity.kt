c0cd1ef22894bf4e0ad79847d542b4b711e330d0
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-07-09 09:55:30 +0300

    Added PokeScout Developer

---
+++ PokeScoutDeveloper/app/src/main/java/com/example/pokescoutdeveloper/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.pokescoutdeveloper
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pokescoutdeveloper.ui.theme.PokeScoutDeveloperTheme
+import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+
+    private var nfcAdapter: NfcAdapter? = null
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var intentFiltersArray: Array<IntentFilter>
+    private lateinit var techListsArray: Array<Array<String>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Nfc stuff
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available on this device", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+
+        pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            PendingIntent.FLAG_MUTABLE,
+        )
+
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
+            try {
+                addDataType("*/*")
+            } catch (e: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException("Failed to add MIME type.", e)
+            }
+        }
+
+        intentFiltersArray = arrayOf(ndef)
+        techListsArray = arrayOf(arrayOf(Ndef::class.java.name))
+
+        // Visuals
+        setContent {
+            PokeScoutDeveloperTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == intent.action ||
+            NfcAdapter.ACTION_TAG_DISCOVERED == intent.action) {
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            tag?.let {
+                writeToTag(it, "Hello, PokeScout!")
+            }
+        }
+    }
+
+    private fun writeToTag(tag: Tag, data: String) {
+        val ndef = Ndef.get(tag)
+        ndef?.let {
+            try {
+                it.connect()
+                val record = createTextRecord(data, Locale.ENGLISH, true)
+                val message = NdefMessage(arrayOf(record))
+                it.writeNdefMessage(message)
+                it.close()
+                Toast.makeText(this, "Written content: $data", Toast.LENGTH_LONG).show()
+                Log.d("NFC", "Written content: $data")
+            } catch (e: Exception) {
+                Toast.makeText(this, "Error writing NFC tag", Toast.LENGTH_LONG).show()
+                Log.e("NFC", "Error writing NFC tag", e)
+            }
+        }
+    }
+
+    private fun createTextRecord(payload: String, locale: Locale, encodeInUtf8: Boolean): NdefRecord {
+        val langBytes = locale.language.toByteArray(Charset.forName("US-ASCII"))
+        val utfEncoding = if (encodeInUtf8) Charset.forName("UTF-8") else Charset.forName("UTF-16")
+        val textBytes = payload.toByteArray(utfEncoding)
+        val utfBit = if (encodeInUtf8) 0 else 1 shl 7
+        val status = (utfBit + langBytes.size).toChar()
+        val baos = ByteArrayOutputStream(1 + langBytes.size + textBytes.size)
+        baos.write(status.toInt())
+        baos.write(langBytes, 0, langBytes.size)
+        baos.write(textBytes, 0, textBytes.size)
+        return NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, ByteArray(0), baos.toByteArray())
+    }
+}
