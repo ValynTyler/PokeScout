ce9d27862cdf930fd625180b6e2a76d8f8305717
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-08-07 18:27:58 +0300

    Fully implemented custom loading indicator

--- app/PokeScout/pokescout/trainer/src/main/java/com/example/trainer/presentation/screens/LoadingScreen.kt
+++ app/PokeScout/pokescout/trainer/src/main/java/com/example/trainer/presentation/screens/LoadingScreen.kt
@@ -1,6 +1,6 @@
 package com.example.trainer.presentation.screens
 
-import androidx.compose.foundation.Image
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +11,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.getDrawable
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
 import com.example.pokemon.presentation.theme.PokeBallDarkGrey
 import com.example.pokemon.presentation.theme.PokeBallGrey
 import com.example.trainer.R
-import com.google.accompanist.drawablepainter.rememberDrawablePainter
 
 @Preview
 @Composable
@@ -29,18 +33,26 @@ fun LoadingScreen() {
             .padding(8.dp)
             .background(color = PokeBallDarkGrey)
     ) {
-        Image(
-            modifier = Modifier.align(Alignment.Center).size(128.dp),
-//            bitmap = ImageBitmap.imageResource(id = R.drawable.loading),
-            painter = rememberDrawablePainter(
-                drawable = getDrawable(
-                    LocalContext.current,
-                    R.drawable.loading,
-                )
-            ),
+
+        val imageLoader = ImageLoader.Builder(LocalContext.current)
+            .components {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
+
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(R.drawable.loading)
+                .build(),
+            imageLoader = imageLoader,
             contentDescription = null,
-//            filterQuality = FilterQuality.None,
-            colorFilter = ColorFilter.tint(PokeBallGrey, BlendMode.SrcIn)
+            filterQuality = FilterQuality.None,
+            colorFilter = ColorFilter.tint(PokeBallGrey, BlendMode.SrcIn),
+            modifier = Modifier.align(Alignment.Center).size(128.dp),
         )
     }
 }
