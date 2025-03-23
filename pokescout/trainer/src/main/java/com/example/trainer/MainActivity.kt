54989745e3229afdae1178592a4613c45d1881cd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-23 21:38:21 +0200

    feat: pikachu!

--- pokescout/trainer/src/main/java/com/example/trainer/MainActivity.kt
+++ pokescout/trainer/src/main/java/com/example/trainer/MainActivity.kt
@@ -8,14 +8,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import com.example.common.ui.Greeting
 import com.example.common.ui.PokemonImage
 import com.example.trainer.ui.theme.PokeScoutTheme
@@ -32,25 +31,16 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                     Column(
-                        verticalArrangement = Arrangement.Center,
+                        verticalArrangement = Arrangement.SpaceAround,
                         horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier.fillMaxSize()
                     ) {
-                        val size = 200.dp
-                        PokemonImage(id = 1, modifier = Modifier
-                            .height(size)
-                            .width(size)
-                            .background(color = Color.DarkGray)
-                        )
-                        PokemonImage(id = 1, modifier = Modifier
-                            .height(size)
-                            .width(size)
-                            .background(color = Color.DarkGray)
-                        )
-                        PokemonImage(id = 1, modifier = Modifier
-                            .height(size)
-                            .width(size)
-                            .background(color = Color.DarkGray)
+                        PokemonImage(
+                            id = 1,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(color = Color.DarkGray)
                         )
                     }
                 }
