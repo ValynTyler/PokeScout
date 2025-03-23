72915a8bb9282e9757fe33ddab312c0b4d260e9c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-23 19:41:24 +0200

    chore: moved greeting logic out of apps

--- pokescout/developer/src/main/java/com/example/developer/MainActivity.kt
+++ pokescout/developer/src/main/java/com/example/developer/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.common.ui.Greeting
 import com.example.developer.ui.theme.PokeScoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,27 +19,11 @@ class MainActivity : ComponentActivity() {
             PokeScoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Developer",
+                        name = "PokeScout Developer",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeScoutTheme {
-        Greeting("Android")
     }
 }
