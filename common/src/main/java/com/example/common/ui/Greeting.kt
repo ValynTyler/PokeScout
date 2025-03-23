4fc72d2a9e7c28e953f96b0f4cd69eabd413514d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-23 19:29:38 +0200

    fix: enable compose for common

---
+++ common/src/main/java/com/example/common/ui/Greeting.kt
@@ -0,0 +1,23 @@
+package com.example.common.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.common.ui.theme.PokeScoutTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PokeScoutTheme {
+        Greeting("Android")
+    }
+}
