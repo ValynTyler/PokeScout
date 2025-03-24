8dc95b1294cf77304fd11331b81991c8cfa6f4a0
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-24 02:53:29 +0200

    chore: clean imports

--- common/src/main/java/com/example/common/ui/PokemonImage.kt
+++ common/src/main/java/com/example/common/ui/PokemonImage.kt
@@ -3,7 +3,6 @@ package com.example.common.ui
 import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.FilterQuality
