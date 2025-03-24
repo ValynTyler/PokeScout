ccf61e500473baa424968ff37c24607f8fb9f26d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-24 03:58:54 +0200

    feat: create PokemonPlaceholder component

---
+++ common/src/main/java/com/example/common/ui/PokemonPlaceholder.kt
@@ -0,0 +1,37 @@
+package com.example.common.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.common.R
+
+@Composable
+fun PokemonPlaceholder (
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.None,
+) {
+    val imageBitmap = ImageBitmap.imageResource(R.drawable.placeholder)
+    Image(
+        bitmap = imageBitmap,
+        filterQuality = FilterQuality.None,
+        contentDescription = "placeholder",
+        contentScale = contentScale,
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun PokemonPlaceholderPreview  () {
+    PokemonPlaceholder(
+        contentScale = ContentScale.FillWidth,
+        modifier = Modifier
+            .fillMaxSize()
+    )
+}
